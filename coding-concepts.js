08fc8833c6ac4f9f2d6027d882d78aff16509f4d
Keoni <joonhyuk5972@gmail.com>
2023-11-26 14:10:16 -1000

    completed assignment

--- coding-concepts.js
+++ coding-concepts.js
@@ -10,24 +10,24 @@
 const colors = ["tangerine", "magenta", "lilac", "daffodil"]
 // console.log(colors.push("indigo"))
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: 5
+// b) Verify and explain: its pushing a 5th parameter to the colors variable.
 
 // --------------------1) What will this log?
 
 const cohort = "LEARN 2023"
 // console.log(cohort.length)
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: itll say the characters in the parenthesis
+// b) Verify and explain: I verified it in a sandbox but yeah there are 10 pieces of data in the statement 'LEARN 2023' so thats how many characters now define cohort
 
 // --------------------2) What will this log?
 
 const greeting = "Hello World!"
 // console.log(greeting[4])
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer:I have no idea, based on what I see here there isnt anything for [4] to reference so nothing will come up?
+// b) Verify and explain: a zero was printed to the log but I do not know why or what was being measured for an output to be 0
 
 // --------------------3) What will this log?
 
@@ -35,21 +35,21 @@ const languages = ["JavaScript", "Ruby", "Python", "C++"]
 const index = 1
 // console.log(languages[index])
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: it might say ruby, or give them all because all these parameters are within 1 collection of data
+// b) Verify and explain: it counted to the second piece of data in languages which, starting wiht 0, makes Ruby the second index
 
 // --------------------4) What will this log?
 
 const weekendDays = ["saturday", "sunday"]
 // console.log(weekendDays.toUpperCase())
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: it will print them in the log as upperCase words
+// b) Verify and explain: i guess since we didnt define toUpperCase as a function my sandbox is telling me weekendDays.toUpperCase is not a function
 
 // --------------------5) What will this log?
 
 const dataTypes = ["number", "string", "Boolean", "undefined"]
 // console.log(typeof dataTypes.length)
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: itll print 4 cause thats how many datatypes have been listed.
+// b) Verify and explain: it only printed the word, number, im curious why there is typof in the command line. my only assumption is that it stops computing after the first true answer so once it calculated the first word of the data type, it just printed that and is ready for its first command?
